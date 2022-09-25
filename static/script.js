7d3d4e5edb2df70649ac1ddf89d7ede42f2820f8
wally-jnyor <arealunar9@gmail.com>
2022-09-25 02:09:35 -0300

    little fixes initial build

--- static/script.js
+++ static/script.js
@@ -1,14 +1,44 @@
-//button ON/OFF pokedex
-let StartPokedex = document.querySelector('.display_btn1');
 // go to/come back  habilidades
 let showList = document.querySelector('.mostrar-habilidades');
 let hideList = document.querySelector('.BackToMain');
 // data container
 let abiliityList = document.querySelector('.habilidades-list');
 let InfosPokemon = document.querySelector('.infos-pokemon');
+//animations
+let StartPokedex = document.querySelector('.display_btn1');
+let navbar = document.querySelector('.navbar');
+let topDisplay = document.querySelector('.top-display');
+let displayBottom = document.querySelector('.display-pokedex');
+let searchform = document.querySelector('.pokeform');
+let leftLeds = document.querySelectorAll('.left-leds');
+let lentLeft = document.querySelector('.lente-left');
 
+let active = 0
 StartPokedex.addEventListener('click', () => {
-    console.log('click')
+    if(active == 0) {
+        topDisplay.style.animationName = 'topDisplayAnimationsOFF'
+        lentLeft.style.animationName = 'LenteAnimationsOFF'
+        InfosPokemon.style.opacity = '0',
+        navbar.style.opacity = '0'
+        displayBottom.style.backgroundImage = 'none'
+        searchform.style.opacity = '0'
+        leftLeds.forEach(element => {
+            element.style.animationName = 'LedsAnimationsOFF';
+        });
+        active = 1
+    }
+    else {
+        topDisplay.style.animationName = 'topDisplayAnimationsON'
+        lentLeft.style.animationName = 'LenteAnimationsON'
+        InfosPokemon.style.opacity = '1',
+        navbar.style.opacity = '1'
+        displayBottom.style.backgroundImage = 'url(/static/assets/pokeBG1.jpg)'
+        searchform.style.opacity = '1'
+        leftLeds.forEach(element => {
+            element.style.animationName = 'LedsAnimationsON';
+        });
+        active = 0
+    }
 })
 
 showList.addEventListener('click', () => {
@@ -26,8 +56,3 @@ hideList.addEventListener('click', () => {
     InfosPokemon.style.display = 'flex';
     InfosPokemon.style.opacity = '1';
 })
-
-console.log(abiliityList)
-
-// obs: resolver problema que desliga a,
-// pokedex ao buscar pokemon.
